0e8260db2cfac7971c727794e174dbcf106b4c1b
joeri.lock <joeri@reachdigital.nl>
2019-11-24 13:45:57 +0100

    calculate co primes

--- encrytpion.py
+++ encrytpion.py
@@ -1,3 +1,19 @@
+p = 2
+q = 7
+n = p*q
 
-def encrypt():
-    return "hello"
+
+def getCoPrimesFromPrimes(a, b):
+    return (a-1)*(b-1)
+
+
+def getCoPrimes(n):
+    numbers = list(range(2, n+1))
+    for i in numbers:
+        if n % i == 0:
+            for ii in range(i, n, i):
+                numbers.remove(ii)
+    return numbers
+
+
+len(getCoPrimes(n)) == getCoPrimesFromPrimes(p, q)
