42b118dab7733c6cdc97d53da3d1f57b440b0fa9
joeri.lock <joeri@reachdigital.nl>
2019-11-30 22:14:35 +0100

    Better get coPrime function

--- app/encrytpion.py
+++ app/encrytpion.py
@@ -2,21 +2,19 @@ p = 2
 q = 7
 n = p*q
 
-test = getCoPrimes(222)
-len(test)
-
-
-list(range(7,221,7))
 
 def getCoPrimesFromPrimes(a, b):
     return (a-1)*(b-1)
+
 
 def getCoPrimes(n):
     numbers = list(range(2, n))
     for i in numbers[:]:  # not sure what [:] does
         if n % i == 0:
-            for ii in range(i, n, i):
-                numbers.remove(ii)                    
+            for ii in numbers[i-2::i]:
+                numbers[ii-2] = 'x'  # there we be some numbers be getting removed twice
+    numbers = list(set(numbers))
+    numbers.remove('x')
     return [1] + numbers
 
 
