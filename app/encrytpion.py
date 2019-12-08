892389414af91bdaa519cb7a03f8ca108b0c748a
joeri.lock <joeri@reachdigital.nl>
2019-12-08 10:23:11 +0100

    Basis for encryption is done

--- app/encrytpion.py
+++ app/encrytpion.py
@@ -1,6 +1,23 @@
+import random
+
+# two random primes
 p = 2
 q = 7
+# mod i use to dycrpt
 n = p*q
+# my public key
+e = 0
+# private Key
+d = 0
+
+
+def getPrivateKey(phiN):
+    return (phiN * random.randint(0, 1000)) - 1
+
+def getPublicKey(p,q):
+    eList = getCommenCoPrimes(p*q)
+    eList.remove(1)
+    return random.choice(eList)
 
 
 def getCoPrimesFromPrimes(a, b):
@@ -12,9 +29,9 @@ def getCoPrimes(n):
     for i in numbers[:]:  # not sure what [:] does
         if n % i == 0:
             for ii in numbers[i-2::i]:
-                numbers[ii-2] = 'x'  # there we be some numbers be getting removed twice
+                numbers[ii-2] = 0  # there we be some numbers be getting removed twice
     numbers = list(set(numbers))
-    numbers.remove('x')
+    numbers.remove(0)
     return [1] + numbers
 
 
