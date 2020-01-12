72fd033bc82dd4482dbda02980263572a421efbe
joeri.lock <joeri@reachdigital.nl>
2020-01-12 10:46:16 +0100

    easier to read cause of prints

--- app/main.py
+++ app/main.py
@@ -1,2 +1,18 @@
-import server
-import client
+from cypher import Cypher
+from encryption import Encryption
+
+print('---enc---')
+enc = Encryption(79, 41)
+
+print("Pub:" + str(enc.pub))
+print("Priv:" + str(enc.private))
+print("mod:" + str(enc.n))
+print("phiN:" + str(enc.phiN))
+
+print('---text---')
+text = 2
+c = Cypher()
+crypt = c.cypherNum(text, enc.pub, enc.n)
+print("plain:" + str(text))
+print("encrypt:" + str(crypt))
+print("decrypt:" + str(c.cypherNum(crypt, enc.private, enc.n)))
