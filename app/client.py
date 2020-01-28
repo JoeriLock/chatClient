85a50cc1b1edce39465d4363d817c35a3a766503
joeri.lock <joeri@reachdigital.nl>
2020-01-28 15:40:43 +0000

    simple working comunication between client server

--- app/client.py
+++ app/client.py
@@ -1,15 +1,15 @@
 import socket
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+host = "127.0.0.1"
 
+port = 5005
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
+s.connect((host, port))
+s.sendall(b'Hello wolrd')
+data = s.recv(1024)
 s.close()
+print('Received', repr(data))
 
-print("received data:" + data)
+
+
+print(str(host))
--- app/server.py
+++ app/server.py
@@ -1,20 +1,25 @@
 import socket
 
-
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
-
+host = ''
+port = 5005
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+s.bind((host, port))
 
+s.listen(1)
 conn, addr = s.accept()
-print 'Connection address:', addr
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if data == 'die':
+print('Conected by', addr)
+while True:
+    try:
+        data = conn.recv(1024)
+        if not data:
+            break
+
+        print ("client says:")
+        print(data)
+        conn.sendall("server says:hi".encode())
+
+    except socket.error:
+        print("Error Occured.")
         break
-    print "received data:", data
-    conn.send(data)  # echo
+
 conn.close()
