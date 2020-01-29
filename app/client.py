f0a1df8b7f982be8ecacf7c526cee7c5fa0461a2
joeri.lock <joeri@reachdigital.nl>
2020-01-29 13:38:34 +0000

    made a class from client

--- app/client.py
+++ app/client.py
@@ -1,15 +1,22 @@
 import socket
 
-host = "127.0.0.1"
+class Client:
 
-port = 5005
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-s.sendall(b'Hello wolrd')
-data = s.recv(1024)
-s.close()
-print('Received', repr(data))
+    host = ""
+    port = ""
+    s = ""
 
+    def __init__(self,host,port):
+        self.host = host
+        self.port = port
+        self.connect()
 
+    def connect(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.host, self.port))
+        self.s.sendall(b'Hello wolrd')
+        data = self.s.recv(1024)
+        self.s.close()
+        print('Received', repr(data))
 
-print(str(host))
+client = Client("127.0.0.1",5005)
