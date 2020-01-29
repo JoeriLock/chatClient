00339ad118f49d16a3722d6f94bad86e9ff90b85
joeri.lock <joeri@reachdigital.nl>
2020-01-29 13:22:37 +0000

    changed to class

--- app/server.py
+++ app/server.py
@@ -1,25 +1,38 @@
+# Server know the private/decryption key
+# Gives pub key to anyone who wants it
+# Does not send messages, only recievs it
+
 import socket
 
-host = ''
-port = 5005
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
+class Server:
+    host = '127.0.0.1'
+    port = 5005
+    s = ''
+    def __init__(self,host,port):
+        self.host = host
+        self.port = port
+        self.startListen()
 
-s.listen(1)
-conn, addr = s.accept()
-print('Conected by', addr)
-while True:
-    try:
-        data = conn.recv(1024)
-        if not data:
-            break
+    def startListen(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.host, self.port))
+        self.s.listen(1)
+        conn, addr = self.s.accept()
+        print('Conected by', addr)
+        while True:
+            try:
+                data = conn.recv(1024)
+                if not data:
+                    break
 
-        print ("client says:")
-        print(data)
-        conn.sendall("server says:hi".encode())
+                print ("client says:")
+                print(data)
+                conn.sendall("server says:hi".encode())
 
-    except socket.error:
-        print("Error Occured.")
-        break
+            except socket.error:
+                print("Error Occured.")
+                break
 
-conn.close()
+        conn.close()
+
+server = Server('127.0.0.1',5005)
