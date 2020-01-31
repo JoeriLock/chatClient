1742b86a194abe519e0f860cfc5c4f518710dc3c
joeri.lock <joeri@reachdigital.nl>
2020-01-31 11:36:39 +0000

    + key check

--- app/client.py
+++ app/client.py
@@ -1,20 +1,30 @@
 import socket
+import sys
+from cypher import Cypher
 
 class Client:
 
     host = ""
     port = ""
     s = ""
+    isKeySet = True
+    pubKeyFile = './pubkey.txt'
 
     def __init__(self,host,port):
         self.host = host
         self.port = port
+        self.cypher = self.getKey()
         self.connect()
+
+    def getKey(self):
+        if(len(sys.argv) > 2):
+            return Cypher(sys.argv[1],sys.arv[2])
+        self.isKeySet = False
 
     def connect(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((self.host, self.port))
-        self.s.sendall(b'Hello wolrd')
+        self.s.sendall('need key')
         data = self.s.recv(1024)
         self.s.close()
         print('Received', repr(data))
