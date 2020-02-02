2f3e3c2c68b1ddfdf08d0d48ced8c55fb779413c
joeri.lock <joeri@reachdigital.nl>
2020-02-02 14:09:33 +0000

    works witch changes to enc.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from cypher import Cypher
 from encryption import Encryption
 
 print('---enc---')
-enc = Encryption(79, 11)
+enc = Encryption(79, 83)
 
 print("Pub:" + str(enc.pub))
 print("Priv:" + str(enc.private))
@@ -10,10 +10,11 @@ print("mod:" + str(enc.n))
 print("phiN:" + str(enc.phiN))
 
 print('---text---')
-text = 'ik ben joeri'
-c = Cypher()
-crypt = c.chyperString(text, enc.pub, enc.n)
+text = 'ik ben joeri' #message
+c = Cypher(enc.pub, enc.n)
+c2 = Cypher(enc.private, enc.n)
+crypt = c.chyperString(text)
 print("plain:" + str(text))
 print("encrypt:")
 print(crypt)
-print("decrypt:" + str(c.deChyperList(crypt, enc.private, enc.n)))
+print("decrypt:" + str(c2.deChyperList(crypt.split(','))))
